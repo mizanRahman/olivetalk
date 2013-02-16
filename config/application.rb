6e656eafb6fc84b9819f396a96aa0e1b3d45e2d1
Isaac Yerushalmi <iyerushalmi@Isaacs-MacBook-Pro.local>
2013-02-15 16:26:07 -0800

    init

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module Ot
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
         
 	config.generators do |g|
  		 g.stylesheets false
