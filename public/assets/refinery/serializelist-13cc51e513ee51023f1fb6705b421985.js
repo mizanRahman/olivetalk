4549a7717002a12a852bcd2843f36472db55b7ef
Mizan Rahman <mizan.cse.buet@gmail.com>
2013-05-03 03:39:58 +0600

    vendor compiled assets

---
+++ public/assets/refinery/serializelist-13cc51e513ee51023f1fb6705b421985.js
@@ -0,0 +1,15 @@
+/**
+ * jQuery Serialize List
+ * Copyright (c) 2009 Mike Botsko, Botsko.net LLC
+ * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
+ * Copyright notice and license must remain intact for legal use
+ * Version .2
+ *
+ * Serialize an unordered or ordered list item. Optional ability
+ * to determine which attributes are included. The serialization
+ * will be read by PHP as a multidimensional array which you may
+ * use for saving state.
+ *
+ * http://github.com/botskonet/jquery.serialize-list
+ */
+(function(t){t.fn.serializelist=function(e){var i,n,s,r,o={attributes:["id"],allow_nest:!0,prepend:"ul",att_regex:!1,is_child:!1},a=t.extend(o,e),l=a.attributes,c="",u=function(t,e){if(!a.att_regex)return e;for(n in a.att_regex)if(a.att_regex[n].att===t)return e.replace(a.att_regex[n].regex,"")};return a.is_child||(a.prepend="&"+a.prepend),this.each(function(e,n){t(n).children().each(function(n,o){if(a.allow_nest||l.length>1)for(s in l)i=u(l[s],t(o).attr(l[s])),c+=a.prepend+"["+e+"]["+n+"]["+l[s]+"]="+i;else i=u(l[0],t(o).attr(l[0])),c+=a.prepend+"["+e+"]["+n+"]="+i;a.allow_nest&&(r=a.prepend+"["+e+"]["+n+"][children]",t(o).children().each(function(){("UL"==this.tagName||"OL"==this.tagName)&&(c+=t(this).serializelist({prepend:r,is_child:!0}))})),n++})}),c}})(jQuery);
