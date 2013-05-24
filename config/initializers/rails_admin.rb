49725101c6cd5f75532e932d3f82796b051fd1f3
Mizan Rahman <mizan.cse.buet@gmail.com>
2013-05-25 03:42:22 +0600

    Site updated at 2013-05-24 21:42:22 UTC

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,13 +1,14 @@
 RailsAdmin.config do |config|
   config.authorize_with :cancan
+  config.included_models = [ "Profile", "Badge", "Badgeship", "Degree", "Job", "Network", "Post", "Resource", "Topic", "Role","Topic","University","User"]
   
-  config.model 'Profile' do
-    object_label_method do
-      :custom_label_method
-    end
-  end
+  # config.model 'Profile' do
+  #   object_label_method do
+  #     :custom_label_method
+  #   end
+  # end
 
-  def custom_label_method
-    "#{self.first_name} #{self.last_name}"
-  end
+  # def custom_label_method
+  #   "#{self.first_name} #{self.last_name}"
+  # end
 end
