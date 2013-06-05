61938cac9ade1bc51b51e4e260663b9b216b503e
Mizan Rahman <mizan.cse.buet@gmail.com>
2013-06-05 22:58:42 +0600

    Site updated at 2013-06-05 16:58:42 UTC

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,18 +52,18 @@ Ot::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   if config.respond_to?(:action_mailer)
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
+    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'gmail.com',
-    :user_name            => 'dev.mizan.rahman@gmail.com',
-    :password             => 'dev1pass2',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  
-  }
+    config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => 'gmail.com',
+      :user_name            => 'dev.mizan.rahman@gmail.com',
+      :password             => 'dev1pass2',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true}
+  end
 
 
   # Enable threaded mode
